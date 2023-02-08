37827476fc57c238c5fc5e48991bb94b4d57182e
unknown <zaaferkenza08@gmail.com>
2023-02-08 09:04:32 +0100

    partie frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <HelloWorld msg="Hello"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+
