630d5b531cf814ede353073b72f3fa11f8ad705f
unknown <zaaferkenza08@gmail.com>
2023-02-16 17:11:14 +0100

    Login

--- frontend/src/components/HomeComponent.vue
+++ frontend/src/components/HomeComponent.vue
@@ -2,7 +2,13 @@
     <h1>hello</h1>
 </template>
 <script>
+import { getCookie } from './../utils/cookies.js';
 export default {
   name: 'HomeComponent',
+  mounted () {
+    if (!getCookie('client')) {
+      this.$router.push('/Login')
+    } 
+  }
 }
 </script>
