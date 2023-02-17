0e0dd8638b1f8e57ae877133eec0a0469e9d961d
unknown <zaaferkenza08@gmail.com>
2023-02-17 20:20:52 +0100

    Home page

--- frontend/src/components/HomeComponent.vue
+++ frontend/src/components/HomeComponent.vue
@@ -1,10 +1,25 @@
 <template>
-    <h1>hello</h1>
+  <div class="test w-full h-screen flex items-center justify-center">
+   <div class="home">
+    <div class="nav rounded-t-lg bg-green-800 text-white">
+      <h1 class="text-center  font-bold text-sm p-4">Welcome {{client}}</h1>
+      <div class="flex justify-between">
+        <a class="mx-4"><i class="fa fa-sign-out"></i></a>
+        <a><i class="fa fa-plus"></i></a></div>
+    </div> 
+    
+   </div>
+  </div>
 </template>
 <script>
 import { getCookie } from './../utils/cookies.js';
 export default {
   name: 'HomeComponent',
+  data() {
+    return {
+      client : getCookie('client')
+    }
+  },
   mounted () {
     if (!getCookie('client')) {
       this.$router.push('/Login')
@@ -12,3 +27,23 @@ export default {
   }
 }
 </script>
+<style scoped>
+@import '~@fortawesome/fontawesome-free/css/all.css';
+  .test{
+    background-image: url('../assets/bg.jpg');
+    background-size: cover;
+    background-position: 0%;
+  }
+  .home{
+    background-color: white;
+    width:80%;
+    height: 60%;
+    border-radius: 20px;
+  }
+  .nav{
+    width: 100%;
+    display :flex ;
+    justify-content: space-around;
+    align-items: center;
+  }
+  </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,4 +4,5 @@ import router from './router'
 import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
 
+
 createApp(App).use(VueSweetalert2).use(router).mount('#app')
